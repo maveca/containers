6514ca8ed9165f93587cac5f9439ea9c4f1bde0c
Aleksander Mavec <aleksander.mavec@adacta.si>
2020-04-04 08:41:23 +0200

    Get-LatestVerions

---
+++ Get-LatestVersion.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+    Receives latest version of scipts
+.DESCRIPTION
+    Receives latest version of scipts from github. They must be connectd.
+.EXAMPLE
+    PS C:\> .\Get-LatetestVerion.ps1
+    It is running git-pull command.
+#>
+
+& git pull
